19788925dd0e0c27a17a7a2a2e755d69202a75ff
Devonjudelson <devonjudelson@outlook.com>
2024-09-12 09:54:29 -0500

    edits

---
+++ Favorite.java
@@ -0,0 +1,14 @@
+import java.net.URL;
+import java.util.Date;
+
+public class Favorite { 
+    private Date date; 
+    private String name; 
+    private URL url; 
+
+    public Favorite(String name, String address) { 
+        this.name = name;
+        this.url = new URL(adress);
+        this.date = new Date();
+    }
+}
